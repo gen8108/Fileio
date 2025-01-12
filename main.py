e51735ef0068a23ad57c528f5f27c8256a41d77d
user <gen8108@mail.ru>
2025-01-12 13:05:23 +0700

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 import requests # запрос на сайт
 
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {'file': open(filepath, 'rb')}
-        response = requests('https://file.io', file=files)
-        if response.status_code == 200:
-            link = response.json()['link']
-            entry.insert(0, link)
-
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files=files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+                print(response)
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
 window = Tk()
 window.title("Сохранение файлов в облаке")
